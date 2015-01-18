616748de48c6fd745963ed4618d7dfe0d72cb24a
Sina <Sina@Sinas-MacBook-Pro.local>
2015-01-18 14:27:38 -0500

    Initial Commit

---
+++ MAPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MAPP
+//
+//  Created by Sina on 1/18/15.
+//  Copyright (c) 2015 MIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
